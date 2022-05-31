279064efdc8fcda3065bc047c8ab32eed38b2cee
Bruno de Lima Oliveira <brunolima@inatel.br>
2022-05-31 12:16:51 -0300

    external component structure

---
+++ components/utils/include/project_config.h
@@ -0,0 +1,38 @@
+/*
+ * project_config.h
+ *
+ *  Created on: 31 de mai de 2022
+ *      Author: bruno
+ */
+
+#ifndef COMPONENTS_UTILS_INCLUDE_PROJECT_CONFIG_H_
+#define COMPONENTS_UTILS_INCLUDE_PROJECT_CONFIG_H_
+
+#include "sdkconfig.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+/* include FreeRTOS */
+#include "freertos/FreeRTOSConfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
+#include "freertos/timers.h"
+
+/* include components */
+#include <inttypes.h>
+#include "esp_log.h"
+
+/* include ESP32 modules */
+#include "esp_err.h"
+
+/* Some scripts may not require all the parameters passed. Use this to avoid
+ * compiler warnings about unused variables. */
+#define UNUSED(x)               (void)(sizeof(x))
+
+#endif /* COMPONENTS_UTILS_INCLUDE_PROJECT_CONFIG_H_ */
--- main/main.c
+++ main/main.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
+/*
+ * main.c
+ *
+ *  Created on: 31 de mai de 2022
+ *      Author: brunolima
+ */
+
+/* Configurations include */
+#include "project_config.h"
+
+#define MAIN_TAG "main"
 
 void app_main(void)
 {
+	ESP_LOGI(MAIN_TAG, "App main !!");
 
+	while (1)
+	{
+        vTaskDelay(pdMS_TO_TICKS(10000));
+    }
 }
