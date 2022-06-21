a0945c5bf49afaa11eb349c3e02cd61d491c86ce
Bruno de Lima Oliveira <brunolima@inatel.br>
2022-06-21 09:56:26 -0300

    added initial uart structure

---
+++ components/rf_module/include/rf_uart.h
@@ -0,0 +1,17 @@
+/*
+ * rf_uart.h
+ *
+ *  Created on: 21 de jun. de 2022
+ *      Author: brunolima
+ */
+
+#ifndef COMPONENTS_RF_MODULE_INCLUDE_RF_UART_H_
+#define COMPONENTS_RF_MODULE_INCLUDE_RF_UART_H_
+
+
+#include "project_config.h"
+
+
+
+
+#endif /* COMPONENTS_RF_MODULE_INCLUDE_RF_UART_H_ */
