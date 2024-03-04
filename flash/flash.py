ac1bc326fcbc500dd5b28ba476e522f2586eef62
PedroBorem <pedro.borem@outlook.com>
2024-03-04 17:58:38 -0300

    [Flash App] Corrigindo Paths para windows

--- flash/flash.py
+++ flash/flash.py
@@ -2,11 +2,13 @@ import subprocess
 import tkinter as tk
 from tkinter import messagebox
 import os
+ROOT = "./flash"
 
 def gravar_firmware(porta_entry):
     porta = porta_entry.get()
-    firmware_path = "v2.0.0"  # Aqui é onde você define a versão do firmware
-    esptool_path = os.path.join(os.getcwd(), "esptool", "esptool.py")
+    firmware_path = f"{ROOT}/v2.0.0"  # Aqui é onde você define a versão do firmware
+    #esptool_path = os.path.join(os.getcwd(), "esptool", "esptool.py")
+    esptool_path = f"{ROOT}/esptool/esptool.py"
     
     if verificar_porta(porta):
         comando = f"python {esptool_path} --chip esp32s3 -p {porta} -b 460800 --before=default_reset --after=hard_reset write_flash --flash_mode dio --flash_freq 80m --flash_size 8MB 0x0 {firmware_path}/bootloader.bin 0x10000 {firmware_path}/fw_PlacaDeControle_2.bin 0x8000 {firmware_path}/partition-table.bin"
@@ -29,7 +31,7 @@ def criar_interface():
     root = tk.Tk()
     root.title("Gravação do ESP32")
 
-    logo = tk.PhotoImage(file=f"./flash/logo.png")
+    logo = tk.PhotoImage(file=f"{ROOT}/logo.png")
     logo_label = tk.Label(root, image=logo)
     logo_label.pack()
 
