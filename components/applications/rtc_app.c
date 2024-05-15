83fc512840dc56102d465c6f20e2c4ab78631738
SoilTecnologia <contato@soiltech.com.br>
2024-05-15 17:19:25 -0300

    Convertendo o timestamp para string para publicar na nuvem

--- components/applications/rtc_app.c
+++ components/applications/rtc_app.c
@@ -107,7 +107,8 @@ bool rtc_app_set_timestamp(time_t timestamp)
 		ESP_LOGE(RTC_APP_TAG, "(%s), Set invalid timestamp (%lld)",__func__, timestamp);
 		LOG_DBG_ERROR(RTC_APP_TAG, "set_invalid_timestamp");
 		vTaskDelay(pdMS_TO_TICKS(2000));
-		LOG_DBG_ERROR(RTC_APP_TAG, timestamp);
+		char *timestamp_str = ctime(&timestamp);
+		LOG_DBG_ERROR(RTC_APP_TAG, timestamp_str);
 	}
 
 	return ret;
