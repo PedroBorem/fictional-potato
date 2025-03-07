a2e38518fbe74d44d31055510c02fb9f0e672251
EduCR03 <eduardocostar03@gmail.com>
2025-03-07 17:16:46 -0300

    Ajuste de versao

--- components/utils/include/project_config.h
+++ components/utils/include/project_config.h
@@ -29,7 +29,7 @@
 /**
  * @brief Firmware version.
  */
-#define CONFIG_FW_VERSION           ("v2.8.0")
+#define CONFIG_FW_VERSION           ("v2.8.2")
 
 /**
  * @brief Maximum number of scheduling values.
