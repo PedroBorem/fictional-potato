4eb6182bdd736e86d437e98b9120debd04bdd103
PedroBorem <pedro.borem@outlook.com>
2025-10-01 13:35:58 -0300

    Update firmware version to v2.8.7

--- components/utils/include/project_config.h
+++ components/utils/include/project_config.h
@@ -29,7 +29,7 @@
 /**
  * @brief Firmware version.
  */
-#define CONFIG_FW_VERSION           ("v2.8.5")
+#define CONFIG_FW_VERSION           ("v2.8.7")
 
 /**
  * @brief Maximum number of scheduling values.
