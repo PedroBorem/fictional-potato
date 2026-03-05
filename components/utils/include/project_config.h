c27f8e2c8d226d5c495d43b8470638b63830116e
EduCR03 <eduardocostar03@gmail.com>
2026-03-05 17:39:13 -0300

    config: bump board test version tag

--- components/utils/include/project_config.h
+++ components/utils/include/project_config.h
@@ -29,7 +29,7 @@
 /**
  * @brief Firmware version.
  */
-#define CONFIG_FW_VERSION           ("rc2.9.0_teste_7")
+#define CONFIG_FW_VERSION           ("rc2.9.0_teste_10")
 
 /**
  * @brief Maximum number of scheduling values.
